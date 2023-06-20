c4d009fd3adcbe1475a9e3fe1a32125e8f09ae31
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-20 17:25:51 +0300

    Update kata_8.js

--- kata_8.js
+++ kata_8.js
@@ -104,7 +104,47 @@ return
 }
 
 //task 9
+/*Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+*/
+function century(year) {
+  let result = 0;
+  for (let i = 0; i < year; i++) {
+    if (i % 100 === 0) {
+      result++;
+    }// Finish this :)
+  }
+  return result;
+}
 //task 10
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"*/
+
+function bmi(weight, height) {
+     let bmi = weight / (height * height)
+  if (bmi <= 18.5 ) {
+    return "Underweight"
+    }else if(bmi <= 25.0 ) {
+return "Normal"
+  } else if(bmi <= 30.0 ){
+    return "Overweight"
+    } else {
+      return "Obese"
+    }
+}
+
 //task 11
 //task 12
 //task 13
