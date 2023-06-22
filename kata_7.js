3b08e687bab1b0ad4c43a1309546577c3502c3b5
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-22 18:25:29 +0300

    Create kata_7.js

---
+++ kata_7.js
@@ -0,0 +1,29 @@
+//task 1
+/*The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock*/
+function sayHello(name) {
+   return "Hello, " + name;
+}
+
+//task 2
+//task 3
+//task 4
+//task 5
+//task 6
+//task 7
+//task 8
+//task 9
+//task 10
+//task 11
+//task 12
+//task 13
+//task 14
+//task 15
+//task 16
+//task 17
+//task 18
+//task 19
+//task 20
