4589cf89c3b03a9c5b9f2bcc131d9e1d7ad9bbeb
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-23 01:32:14 +0300

    Update kata_7.js

--- kata_7.js
+++ kata_7.js
@@ -9,7 +9,40 @@ function sayHello(name) {
 }
 
 //task 2
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]*/
+
+function greet(name){
+  return "Hello, " + name + " how are you doing today?"//your code here
+}
+
 //task 3
+/*Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.*/ 
+
+function getGrade (s1, s2, s3) {
+   let score = (s1 + s2 + s3) / 3; // средний бал
+ if (90 <= score && score  <= 100){ //диапазон
+   return "A"
+ }else if (80 <= score && score  <= 90){
+   return "B"
+           }else if (70 <= score && score  <= 80){
+   return "C"
+                     }else if (60 <= score && score  <= 70){
+   return "D"
+   }else{
+   return "F"
+   }// Code here
+}
 //task 4
 //task 5
 //task 6
