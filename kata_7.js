b11c7d61a84a26deed26dcdaaab5e95dd1e2c7d4
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-24 00:48:11 +0300

    Update kata_7.js

--- kata_7.js
+++ kata_7.js
@@ -67,10 +67,44 @@ function getAge(inputString){
 }
 
 //task 6
-/**/
+/*Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
 
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+Try to do it without using if statements!*/
+
+
+function arithmetic(a, b, operator){
+  if (operator === 'add') return a + b;
+  if (operator === 'subtract') return a - b;
+  if (operator === 'multiply') return a * b;
+  if (operator === 'divide') return a / b;
+  
+  //your code here!
+}
 
 //task 7
+/**/
+function finalGrade (exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  }
+  return 0;
+      // final grade
+}
+
 //task 8
 //task 9
 //task 10
