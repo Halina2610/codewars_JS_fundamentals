d94cdd032542cb167b2b27f98c0ff687ec7e605e
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-30 02:44:59 +0300

    Create kata_6.js

---
+++ kata_6.js
@@ -0,0 +1,56 @@
+//Solutions 1
+/*Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5*/
+
+function litres(time) {
+ return Math. floor(time * 0.5)
+}
+//Solutions 2
+/**/
+
+//Solutions 3
+/**/
+
+//Solutions 4
+/**/
+//Solutions 5
+/**/
+//Solutions 6
+/**/
+//Solutions 7
+/**/
+//Solutions 8
+/**/
+//Solutions 9
+/**/
+//Solutions 10
+/**/
+//Solutions 11
+/**/
+//Solutions 12
+/**/
+//Solutions 13
+/**/
+//Solutions 14
+/**/
+//Solutions 15
+/**/
+//Solutions 16
+/**/
+//Solutions 17
+/**/
+//Solutions 18
+/**/
+//Solutions 19
+/**/
