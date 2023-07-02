5e02294c8a6b7ae25fa6bc29a337aca3b70776b2
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-07-03 00:39:27 +0300

    Update 8_kyu_2.js

--- 8_kyu_2.js
+++ 8_kyu_2.js
@@ -134,9 +134,25 @@ function squareSum(numbers) {
 }
 
 //Solutions 9
-/**/
+/*Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234*/
+
+const stringToNumber = function(str){
+ return Number(str); // put your code here
+}
+
 //Solutions 10
 /**/
+
+
+
 //Solutions 11
 /**/
 //Solutions 12
