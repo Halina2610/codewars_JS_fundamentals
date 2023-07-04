de3679bd6cb1eb7da30c7fafff5061f63b882a6a
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-07-04 23:35:37 +0300

    Update 8_kyu_2.js

--- 8_kyu_2.js
+++ 8_kyu_2.js
@@ -299,6 +299,69 @@ function reverseList(list) {
 return list.reverse(" ")
 }
 //Solutions 18
-/**/
+/*Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.*/
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let twiceAs = dadYearsOld - sonYearsOld * 2;
+return twiceAs < 0 ? twiceAs * (-1) : twiceAs;// your code here
+}
+
 //Solutions 19
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
+
+function solution(str){
+  return str.split('').reverse().join(''); 
+  }
+
+//Solutions 20
+/*Exclusive "or" (xor) Logical Operator
+Overview
+In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" 
+(hence the name of this Kata). The exclusive or evaluates two booleans.
+It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+false xor false == false // since both are false
+true xor false == true // exactly one of the two expressions are true
+false xor true == true // exactly one of the two expressions are true
+true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+Task
+Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) 
+where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly 
+one of the two expressions evaluate to true, false otherwise.*/
+
+function xor(a, b) {
+  if (a == true && b == false) return true;
+  if (a == false && b == true) return true;
+  return false;// TODO: Program Me
+}
+  
+
+//Solutions 21
+/**/
+//Solutions 22
+/**/
+//Solutions 23
+/**/
+//Solutions 24
+/**/
+//Solutions 25
+/**/
+//Solutions 26
+/**/
+//Solutions 27
+/**/
+//Solutions 28
+/**/
+//Solutions 29
+/**/
+//Solutions 30
 /**/
