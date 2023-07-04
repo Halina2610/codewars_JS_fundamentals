7b92da67432382b9c33e729ad20925cb20700a06
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-07-04 18:46:09 +0300

    Update 8_kyu_2.js

--- 8_kyu_2.js
+++ 8_kyu_2.js
@@ -270,9 +270,34 @@ function stringToArray(string){
 }
 
 //Solutions 16
-/**/
+/*There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  let result = (classPoints.reduce((sum,current)=>sum+current,0)+ yourPoints)/classPoints.length+1;
+  if (yourPoints > result) return true;
+  if (yourPoints < result) return false;
+    // Your code here
+} 
+
+
 //Solutions 17
-/**/
+/*In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]*/
+
+function reverseList(list) {
+return list.reverse(" ")
+}
 //Solutions 18
 /**/
 //Solutions 19
