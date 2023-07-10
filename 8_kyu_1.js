3b0ac2127fe83f72ea54fd8d165c0928f14c16b8
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-07-10 23:57:12 +0300

    Update 8_kyu_1.js

--- 8_kyu_1.js
+++ 8_kyu_1.js
@@ -590,7 +590,17 @@ function descendingOrder(n){
    //перевернуть массив, соеденить массив чисел в одно число .join('')
 }
 //task 34
-/**/
+/*Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful,
+there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'*/
+function smash (words) {
+  let  sentence = words.join(" ")
+      return  sentence 
+};
 //task 35
 /**/
 //task 36
